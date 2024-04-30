81b937b14f9ebd058e094afa6104914f62ae0f51
healermikado <pepin.remi@gmail.com>
2024-04-30 11:32:26 +0200

    fix tf

---
+++ teraform/main_serverless.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from constructs import Construct
+from cdktf import App, TerraformStack, TerraformOutput
+from cdktf_cdktf_provider_aws.provider import AwsProvider
+from cdktf_cdktf_provider_aws.default_vpc import DefaultVpc
+from cdktf_cdktf_provider_aws.default_subnet import DefaultSubnet
+from cdktf_cdktf_provider_aws.lambda_function import LambdaFunction
+from cdktf_cdktf_provider_aws.lambda_permission import LambdaPermission
+from cdktf_cdktf_provider_aws.data_aws_caller_identity import DataAwsCallerIdentity
+from cdktf_cdktf_provider_aws.s3_bucket import S3Bucket, S3BucketCorsRule
+from cdktf_cdktf_provider_aws.s3_bucket_notification import S3BucketNotification
+
+class ServerlessStack(TerraformStack):
+    def __init__(self, scope: Construct, id: str):
+        super().__init__(scope, id)
+        AwsProvider(self, "AWS", region="us-east-1")
+        account_id = DataAwsCallerIdentity(self, "acount_id").account_id
+        bucket = S3Bucket()
+
+        lambda_function = LambdaFunction()
+
+        permission = LambdaPermission()
+
+        notification = S3BucketNotification()
+
+        TerraformOutput()
+        
+        TerraformOutput()
+
+app = App()
+ServerlessStack(app, "cdktf_serverless")
+app.synth()
+
