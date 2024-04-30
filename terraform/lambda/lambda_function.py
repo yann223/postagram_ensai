bd58a97f328f81757b7638ea371072c671fba055
healermikado <pepin.remi@gmail.com>
2024-04-30 12:41:22 +0200

    serverless

--- terraform/lambda/lambda_function.py
+++ terraform/lambda/lambda_function.py
@@ -1,6 +1,15 @@
 import json
+from urllib.parse import unquote_plus
 import boto3
 import os
+import logging
+print('Loading function')
+logger = logging.getLogger()
+logger.setLevel("INFO")
+s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+reckognition = boto3.client('rekognition')
+
+table = dynamodb.Table(os.getenv("table"))
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
--- terraform/main_serverless.py
+++ terraform/main_serverless.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python
 from constructs import Construct
-from cdktf import App, TerraformStack, TerraformOutput
+from cdktf import App, TerraformStack, TerraformOutput, TerraformAsset, AssetType
 from cdktf_cdktf_provider_aws.provider import AwsProvider
 from cdktf_cdktf_provider_aws.default_vpc import DefaultVpc
 from cdktf_cdktf_provider_aws.default_subnet import DefaultSubnet
 from cdktf_cdktf_provider_aws.lambda_function import LambdaFunction
 from cdktf_cdktf_provider_aws.lambda_permission import LambdaPermission
 from cdktf_cdktf_provider_aws.data_aws_caller_identity import DataAwsCallerIdentity
-from cdktf_cdktf_provider_aws.s3_bucket import S3Bucket, S3BucketCorsRule
-from cdktf_cdktf_provider_aws.s3_bucket_notification import S3BucketNotification
-
+from cdktf_cdktf_provider_aws.s3_bucket import S3Bucket
+from cdktf_cdktf_provider_aws.s3_bucket_cors_configuration import S3BucketCorsConfiguration, S3BucketCorsConfigurationCorsRule
+from cdktf_cdktf_provider_aws.s3_bucket_notification import S3BucketNotification, S3BucketNotificationLambdaFunction
+from cdktf_cdktf_provider_aws.dynamodb_table import DynamodbTable, DynamodbTableAttribute
 class ServerlessStack(TerraformStack):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
         AwsProvider(self, "AWS", region="us-east-1")
+
         account_id = DataAwsCallerIdentity(self, "acount_id").account_id
+        
         bucket = S3Bucket()
+
+        S3BucketCorsConfiguration(
+            self, "cors",
+            bucket=bucket.id,
+            cors_rule=[S3BucketCorsConfigurationCorsRule(
+                allowed_headers = ["*"],
+                allowed_methods = ["GET", "HEAD", "PUT"],
+                allowed_origins = ["*"]
+            )]
+            )
+        dynamo_table = DynamodbTable()
+
+        # Packagage du code
+        code = TerraformAsset()
 
         lambda_function = LambdaFunction()
 
@@ -24,7 +41,7 @@ class ServerlessStack(TerraformStack):
         notification = S3BucketNotification()
 
         TerraformOutput()
-        
+
         TerraformOutput()
 
 app = App()
